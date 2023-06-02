5a9cd8d94fbdecdf1bf33e1557e308a951e6e69e
iftekharishti1999 <83029429+iftekharishti1999@users.noreply.github.com>
2023-06-02 11:42:18 +0600

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,7 @@
+import urllib
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+page = 'https://www.healthboards.com/boards/brain-tumors/1041943-worried-about-having-tumor.html'
+request =urllib.Request()
